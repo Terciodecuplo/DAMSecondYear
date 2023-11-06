af721b94ba8142821eae389572c29c9e895ea181
Jose Murcia <112321922+Terciodecuplo@users.noreply.github.com>
2023-11-06 18:17:23 +0100

    Added windowbuilder project (waiting for teacher's answer about compatibility)

---
+++ Modulos/DI/Tema_1/Ejercicios/VentanaVentas/VentanaVentas/src/MainFrame.java
@@ -0,0 +1,65 @@
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
+
+public class MainFrame extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainFrame frame = new MainFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 572, 278);
+		contentPane = new JPanel();
+		contentPane.setBorder(new LineBorder(new Color(150, 154, 152)));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblVentas = new JLabel("Ventas");
+		lblVentas.setBounds(196, 85, 116, 41);
+		lblVentas.setFont(new Font("Tahoma", Font.BOLD, 34));
+		contentPane.add(lblVentas);
+		
+		JLabel lblCliente = new JLabel("Cliente:");
+		lblCliente.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblCliente.setBounds(82, 177, 61, 16);
+		contentPane.add(lblCliente);
+		
+		textField = new JTextField();
+		textField.setBounds(155, 166, 186, 38);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnBuscar = new JButton("Buscar");
+		btnBuscar.setBounds(342, 166, 117, 41);
+		contentPane.add(btnBuscar);
+	}
+}
