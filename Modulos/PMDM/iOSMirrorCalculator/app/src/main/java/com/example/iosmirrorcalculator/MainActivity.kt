9e83940290e985e1c72be7131d7a2b68011686b5
Jose Murcia <112321922+Terciodecuplo@users.noreply.github.com>
2023-11-10 20:53:05 +0100

    added id to every element and set the binding in the main project

--- Modulos/PMDM/iOSMirrorCalculator/app/src/main/java/com/example/iosmirrorcalculator/MainActivity.kt
+++ Modulos/PMDM/iOSMirrorCalculator/app/src/main/java/com/example/iosmirrorcalculator/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.iosmirrorcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import com.example.iosmirrorcalculator.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.acBtn!!.setOnClickListener(this)
+
+    }
+
+    override fun onClick(p0: View?) {
+        when(p0?.id){
+            binding.acBtn!!.id->{
+                binding.resultText.text = "0"
+            }
+
+        }
     }
 }
