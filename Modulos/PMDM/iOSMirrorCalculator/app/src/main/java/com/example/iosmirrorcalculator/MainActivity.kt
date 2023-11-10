326d826f5b7038782f8c731f7840710d69b436f0
Jose Murcia <112321922+Terciodecuplo@users.noreply.github.com>
2023-11-10 22:17:30 +0100

    added the initial value for each numeric input that has to be represented on the result text field

--- Modulos/PMDM/iOSMirrorCalculator/app/src/main/java/com/example/iosmirrorcalculator/MainActivity.kt
+++ Modulos/PMDM/iOSMirrorCalculator/app/src/main/java/com/example/iosmirrorcalculator/MainActivity.kt
@@ -15,16 +15,59 @@ class MainActivity : AppCompatActivity(), OnClickListener {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.acBtn!!.setOnClickListener(this)
-
+        binding.acBtn.setOnClickListener(this)
+        binding.zeroNumberBtn.setOnClickListener(this)
+        binding.oneNumberBtn.setOnClickListener(this)
+        binding.twoNumberBtn.setOnClickListener(this)
+        binding.threeNumberBtn.setOnClickListener(this)
+        binding.fourNumberBtn.setOnClickListener(this)
+        binding.fiveNumberBtn.setOnClickListener(this)
+        binding.sixNumberBtn.setOnClickListener(this)
+        binding.sevenNumberBtn.setOnClickListener(this)
+        binding.eightNumberBtn.setOnClickListener(this)
+        binding.nineNumberBtn.setOnClickListener(this)
+        binding.divisionBtn.setOnClickListener(this)
+        binding.additionBtn.setOnClickListener(this)
+        binding.multiplyBtn.setOnClickListener(this)
+        binding.sustractionBtn.setOnClickListener(this)
+        binding.equalBtn.setOnClickListener(this)
     }
 
     override fun onClick(p0: View?) {
         when(p0?.id){
-            binding.acBtn!!.id->{
+            binding.acBtn.id->{
                 binding.resultText.text = "0"
             }
-
+            binding.oneNumberBtn.id->{
+                binding.resultText.text = "1"
+            }
+            binding.twoNumberBtn.id->{
+                binding.resultText.text = "2"
+            }
+            binding.threeNumberBtn.id->{
+                binding.resultText.text = "3"
+            }
+            binding.fourNumberBtn.id->{
+                binding.resultText.text = "4"
+            }
+            binding.fiveNumberBtn.id->{
+                binding.resultText.text = "5"
+            }
+            binding.sixNumberBtn.id->{
+                binding.resultText.text = "6"
+            }
+            binding.sevenNumberBtn.id->{
+                binding.resultText.text = "7"
+            }
+            binding.eightNumberBtn.id->{
+                binding.resultText.text = "8"
+            }
+            binding.nineNumberBtn.id->{
+                binding.resultText.text = "9"
+            }
+            binding.zeroNumberBtn.id->{
+                binding.resultText.text = "0"
+            }
         }
     }
 }
