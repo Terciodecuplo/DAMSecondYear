3d761d0414e5a8afb71de0d214370896741ec157
Jose Murcia <112321922+Terciodecuplo@users.noreply.github.com>
2023-11-12 09:07:09 +0100

    added all operations, and basic situations on vertical view. now you can use decimals, division by zero, percentages and negative numbers

--- Modulos/PMDM/iOSMirrorCalculator/app/src/main/java/com/example/iosmirrorcalculator/MainActivity.kt
+++ Modulos/PMDM/iOSMirrorCalculator/app/src/main/java/com/example/iosmirrorcalculator/MainActivity.kt
@@ -1,73 +1,265 @@
 package com.example.iosmirrorcalculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.View.OnClickListener
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.iosmirrorcalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
+    private var firstNumberToOperate = 0.0
+    private var secondNumberToOperate = 0.0
+    private var secondNumberToOperateStored = 0.0
+    private var acPressed = false
+    private var operationSelected = ""
+    private var lastOperationSelected = ""
+    private var dividedByZero = false
+    private val operationButtonsIds = setOf(
+        R.id.sumBtn,
+        R.id.sustractionBtn,
+        R.id.multiplyBtn,
+        R.id.divisionBtn
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val allButtonsBinds = setOf(
+            binding.acBtn,
+            binding.zeroNumberBtn,
+            binding.oneNumberBtn,
+            binding.twoNumberBtn,
+            binding.threeNumberBtn,
+            binding.fourNumberBtn,
+            binding.fiveNumberBtn,
+            binding.sixNumberBtn,
+            binding.sevenNumberBtn,
+            binding.eightNumberBtn,
+            binding.nineNumberBtn,
+            binding.signBtn,
+            binding.percentBtn,
+            binding.decimalBtn,
+            binding.equalBtn,
+            binding.sumBtn,
+            binding.sustractionBtn,
+            binding.multiplyBtn,
+            binding.divisionBtn
+        )
 
-        binding.acBtn.setOnClickListener(this)
-        binding.zeroNumberBtn.setOnClickListener(this)
-        binding.oneNumberBtn.setOnClickListener(this)
-        binding.twoNumberBtn.setOnClickListener(this)
-        binding.threeNumberBtn.setOnClickListener(this)
-        binding.fourNumberBtn.setOnClickListener(this)
-        binding.fiveNumberBtn.setOnClickListener(this)
-        binding.sixNumberBtn.setOnClickListener(this)
-        binding.sevenNumberBtn.setOnClickListener(this)
-        binding.eightNumberBtn.setOnClickListener(this)
-        binding.nineNumberBtn.setOnClickListener(this)
-        binding.divisionBtn.setOnClickListener(this)
-        binding.additionBtn.setOnClickListener(this)
-        binding.multiplyBtn.setOnClickListener(this)
-        binding.sustractionBtn.setOnClickListener(this)
-        binding.equalBtn.setOnClickListener(this)
+        allButtonsBinds.forEach { button -> button.setOnClickListener(this) }
+
     }
 
-    override fun onClick(p0: View?) {
-        when(p0?.id){
-            binding.acBtn.id->{
+    override fun onClick(buttonPressed: View?) {
+        resetOperationSelectedColours()
+        when (buttonPressed?.id) {
+
+            binding.acBtn.id -> {
+                acPressed = true
+                dividedByZero = false
+                firstNumberToOperate = 0.0
+                secondNumberToOperate = 0.0
+                secondNumberToOperateStored = 0.0
+                lastOperationSelected = ""
                 binding.resultText.text = "0"
             }
-            binding.oneNumberBtn.id->{
-                binding.resultText.text = "1"
+
+            binding.oneNumberBtn.id -> updateNumberSequence("1")
+
+            binding.twoNumberBtn.id -> updateNumberSequence("2")
+
+            binding.threeNumberBtn.id -> updateNumberSequence("3")
+
+            binding.fourNumberBtn.id -> updateNumberSequence("4")
+
+            binding.fiveNumberBtn.id -> updateNumberSequence("5")
+
+            binding.sixNumberBtn.id -> updateNumberSequence("6")
+
+            binding.sevenNumberBtn.id -> updateNumberSequence("7")
+
+            binding.eightNumberBtn.id -> updateNumberSequence("8")
+
+            binding.nineNumberBtn.id -> updateNumberSequence("9")
+
+            binding.zeroNumberBtn.id -> updateNumberSequence("0")
+
+            binding.decimalBtn.id -> {
+                if (!binding.resultText.text.toString().contains(".")) {
+                    binding.resultText.append(".")
+                }
             }
-            binding.twoNumberBtn.id->{
-                binding.resultText.text = "2"
+
+            binding.sumBtn.id -> {
+                changeOperationSelectedColour(findViewById(buttonPressed.id))
+                storeFirstOperator()
+                resetDisplay()
+                operationSelected = "+"
+                lastOperationSelected = "+"
             }
-            binding.threeNumberBtn.id->{
-                binding.resultText.text = "3"
+
+            binding.sustractionBtn.id -> {
+                changeOperationSelectedColour(findViewById(buttonPressed.id))
+                storeFirstOperator()
+                resetDisplay()
+                operationSelected = "-"
+                lastOperationSelected = "-"
             }
-            binding.fourNumberBtn.id->{
-                binding.resultText.text = "4"
+
+            binding.multiplyBtn.id -> {
+                changeOperationSelectedColour(findViewById(buttonPressed.id))
+                storeFirstOperator()
+                resetDisplay()
+                operationSelected = "x"
+                lastOperationSelected = "x"
             }
-            binding.fiveNumberBtn.id->{
-                binding.resultText.text = "5"
+
+            binding.divisionBtn.id -> {
+                changeOperationSelectedColour(findViewById(buttonPressed.id))
+                storeFirstOperator()
+                resetDisplay()
+                operationSelected = "/"
+                lastOperationSelected = "/"
             }
-            binding.sixNumberBtn.id->{
-                binding.resultText.text = "6"
+
+            binding.signBtn.id -> {
+                var changeSign = binding.resultText.text.toString()
+                changeSign = if (changeSign[0] == '-') {
+                    changeSign.substring(1)
+                } else {
+                    "-$changeSign"
+                }
+                binding.resultText.text = changeSign
+
             }
-            binding.sevenNumberBtn.id->{
-                binding.resultText.text = "7"
+
+            binding.percentBtn.id -> {
+                var percentage = binding.resultText.text.toString().toDouble()
+                percentage /= 100
+                if (percentage != percentage.toInt().toDouble()) {
+                    binding.resultText.text = percentage.toString()
+                } else {
+                    binding.resultText.text = percentage.toInt().toString()
+                }
             }
-            binding.eightNumberBtn.id->{
-                binding.resultText.text = "8"
+
+            binding.equalBtn.id -> {
+                if (operationSelected == "") {
+                    storeFirstOperator()
+                } else {
+                    storeSecondOperator()
+                }
+                operate()
+                firstNumberToOperate = 0.0
+                secondNumberToOperate = 0.0
+                operationSelected = ""
             }
-            binding.nineNumberBtn.id->{
-                binding.resultText.text = "9"
+        }
+        buttonPressed?.let {
+            if (it is Button && it.id in operationButtonsIds) {
+                changeOperationSelectedColour(it)
             }
-            binding.zeroNumberBtn.id->{
-                binding.resultText.text = "0"
+        }
+
+    }
+
+    private fun changeOperationSelectedColour(button: Button) {
+        button.setBackgroundColor(ContextCompat.getColor(this, R.color.white))
+        button.setTextColor(ContextCompat.getColor(this, R.color.orange))
+    }
+
+    private fun resetOperationSelectedColours() {
+        operationButtonsIds.forEach { buttonId ->
+            findViewById<Button>(buttonId).setBackgroundColor(
+                (ContextCompat.getColor(
+                    this,
+                    R.color.orange
+                ))
+            )
+            findViewById<Button>(buttonId).setTextColor(
+                (ContextCompat.getColor(
+                    this,
+                    R.color.white
+                ))
+            )
+        }
+
+    }
+
+    private fun operate() {
+        var result = 0.0
+        if (!dividedByZero) {
+            if (operationSelected != "") {
+                secondNumberToOperateStored = secondNumberToOperate
+            }
+            if (operationSelected == "" && lastOperationSelected != "") {
+                operationSelected = lastOperationSelected
+                secondNumberToOperate = secondNumberToOperateStored
+            }
+
+            when (operationSelected) {
+                "+" -> {
+                    result = firstNumberToOperate + secondNumberToOperate
+                }
+
+                "-" -> {
+                    result = firstNumberToOperate - secondNumberToOperate
+                }
+
+                "x" -> {
+                    result = firstNumberToOperate * secondNumberToOperate
+
+                }
+
+                "/" -> {
+                    result = firstNumberToOperate / secondNumberToOperate
+
+                }
+            }
+        } else {
+            binding.resultText.text = getString(R.string.dividedByZeroError)
+        }
+        operationSelected = ""
+        if (binding.resultText.text != getString(R.string.dividedByZeroError)) {
+            if (result != result.toInt().toDouble()) {
+                binding.resultText.text = result.toString()
+            } else {
+                binding.resultText.text = result.toInt().toString()
             }
         }
     }
+
+    private fun storeSecondOperator() {
+        secondNumberToOperate = binding.resultText.text.toString().toDouble()
+        if (secondNumberToOperate == 0.0 && operationSelected == "/") {
+            dividedByZero = true
+        }
+    }
+
+    private fun resetDisplay() {
+        acPressed = true
+    }
+
+    private fun storeFirstOperator() {
+        if (binding.resultText.text != getString(R.string.dividedByZeroError)) {
+            firstNumberToOperate = binding.resultText.text.toString().toDouble()
+        }
+    }
+
+    private fun updateNumberSequence(numberPressed: String) {
+        if (acPressed || binding.resultText.text == "0") {
+            binding.resultText.text = numberPressed
+            acPressed = false
+        } else {
+            binding.resultText.append(numberPressed)
+        }
+    }
+
+
 }
